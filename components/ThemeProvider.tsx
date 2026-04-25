95f2a1bc4983da4fc5bc473b9fcee1907235fc96
XSinh1 <24126191@student.hcmute.edu.vn>
2026-04-25 18:40:13 +0700

    ThemeProvider.tsx

---
+++ components/ThemeProvider.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  attribute?: string | string[]; 
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+}
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const NextThemesProviderCast = NextThemesProvider as unknown as React.ComponentType<ThemeProviderProps>;
+
+  return (
+    <NextThemesProviderCast 
+      attribute="class" 
+      defaultTheme="light" 
+      enableSystem={false}
+      {...props}
+    >
+      {children}
+    </NextThemesProviderCast>
+  );
+}
